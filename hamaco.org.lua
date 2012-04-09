8d846e08896ececcdf7961218c8b5cac62b7db1d
hamaco <hamanaka.kazuhiro@gmail.com>
2012-04-09 18:05:50 +0900

    色々今使ってるの追加

--- hamaco.org.lua
+++ hamaco.org.lua
@@ -1,2 +1,8 @@
+a("remilia.hamaco.org", "59.106.175.183")
+a("frandre.hamaco.org", "182.48.55.109")
+
+a("git.hamaco.org", "59.106.175.183")
+a("redmine.hamaco.org", "59.106.175.183")
+
 a("blog.hamaco.org", "182.48.55.109")
 a("labs.hamaco.org", "182.48.55.109")
