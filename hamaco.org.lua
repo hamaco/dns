0cabd8a045d02a67b01c32b9968dce42f9827c79
hamaco <hamanaka.kazuhiro@gmail.com>
2012-04-21 12:41:47 +0900

    redmine.hamaco.orgのIP変更

--- hamaco.org.lua
+++ hamaco.org.lua
@@ -2,7 +2,7 @@ a("remilia.hamaco.org", "59.106.175.183")
 a("frandre.hamaco.org", "182.48.55.109")
 
 a("git.hamaco.org", "59.106.175.183")
-a("redmine.hamaco.org", "59.106.175.183")
+a("redmine.hamaco.org", "133.242.52.211")
 
 a("blog.hamaco.org", "182.48.55.109")
 a("labs.hamaco.org", "182.48.55.109")
