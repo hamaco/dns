3d5881aadd49c24f4a3d694ca4ddde4bd59518f9
hamaco <hamanaka.kazuhiro@gmail.com>
2013-05-28 19:49:15 +0900

    remove non used subdomains

--- hamaco.org.lua
+++ hamaco.org.lua
@@ -1,14 +1,6 @@
 a("remilia", "59.106.175.183")
-a("frandre", "182.48.55.109")
-
-a("mail", "133.242.48.92")
-mx("mail", "mail.hamaco.org", 10)
-
-a("git", "59.106.175.183")
-a("redmine", "133.242.49.144")
 
 cname("blog", "hamaco.github.com")
 a("labs", "219.94.252.20")
-a("candycane", "219.94.252.20")
 
 a("local", "127.0.0.1")
