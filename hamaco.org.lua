6501f2587b01d0ae5b9724a4e240bc8d27daa816
hamaco <hamanaka.kazuhiro@gmail.com>
2015-01-13 11:50:04 +0900

    diary

--- hamaco.org.lua
+++ hamaco.org.lua
@@ -1,5 +1,7 @@
+cname("blog", "hamaco.github.com")
+cname("diary", "domains.tumblr.com")
+
 a("remilia", "59.106.175.183")
 
-cname("blog", "hamaco.github.com")
 a("labs", "219.94.252.20")
 a("imas-ml", "219.94.252.20")
